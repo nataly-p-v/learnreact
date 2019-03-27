0297cad8f5143b8f7d960e0ac26b0b9de327cfb9
Nataliia_Pachkovska <Nataliia_Pachkovska@epam.com>
2019-03-27 15:56:28 +0200

    mock up

--- src/components/searchResult/searchResult.js
+++ src/components/searchResult/searchResult.js
@@ -1,29 +1,88 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import SearchHeader from '../searchHeader/searchHeader';
 
 class resultPage extends Component {
-    render () {
+    render() {
         return (
-           <div className={"search-result"}>
-                <header className={"search-result__header"}>
-                    <div className="search-result__header--title">12 movies found</div>
-                    <div className="search-result__header--title">Sort by release date rating</div>
-                </header>
-               <div className="search-result__grid clearfix">
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/></div><span>asd</span></div>
-                   <div className={"search-result__grid-item"}><div className="img-wrapper"><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg" alt=""/></div><span>asd</span></div>
-               </div>
-           </div>
+            <div className={"search-result"}>
+               <SearchHeader sortBy={"sortBy"}/>
+                <div className="search-result__grid clearfix">
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/>
+                        </div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Kill Bill: Vol.2</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>2004</div>
+                        </div>
+                    </div>
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg"
+                            alt=""/></div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Oscar-winning movies</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>1994</div>
+                        </div>
+                    </div>
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/>
+                        </div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Kill Bill: Vol.2</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>2004</div>
+                        </div>
+                    </div>
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg"
+                            alt=""/></div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Oscar-winning movies</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>1994</div>
+                        </div>
+                    </div>
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://images-na.ssl-images-amazon.com/images/I/41CAtQyKWLL._SY445_.jpg" alt=""/>
+                        </div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Kill Bill: Vol.2</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>2004</div>
+                        </div>
+                    </div>
+                    <div className={"search-result__grid-item"}>
+                        <div className="img-wrapper"><img
+                            src="https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg"
+                            alt=""/></div>
+                        <div className={"item-wrapper"}>
+                            <div className="text">
+                                <div className={"item-title"}>Oscar-winning movies</div>
+                                <span>Action & Adventure</span>
+                            </div>
+                            <div className={"year"}>1994</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
 export default resultPage;
